c069dc4d2aba1e352391087523883827050ecf28
Arthur TOUPART <atoupart@e1r4p6.42.fr>
2016-02-26 16:49:59 +0100

    prout

--- get_next_line.c
+++ get_next_line.c
@@ -6,68 +6,76 @@
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 13:27:21 by atoupart          #+#    #+#             */
-/*   Updated: 2016/02/26 00:42:30 by atoupart         ###   ########.fr       */
+/*   Updated: 2016/02/26 16:49:31 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
+
 int			get_next_line(int const fd, char **line)
 {
 	int				ret;
-	char			buf[BUFF_SIZE + 1];
-	static char		*str;
-	static int		i;
-
-	i = -1;
-	while (ret != 0)
-		if ((ret = read(fd, buf, BUFF_SIZE)) == -1)
-			return (-1);
-	pts(buf);
-	str = (char*)malloc(sizeof(char) * ft_strlen(buf));
-	ptn(ret);ptcn;
-	str = ft_strjoin(str, buf);
-	while (str[++i] != '\n')
-		(*line)[i] = str[i];
-	if (str[i] == '\n')
-		return (1);
-	return (0);
-}
-*/
-
-static void		search_n(int const fd, char *str) // surement plus de parametre
-{
-
-
-int				get_next_line(int const fd, char **line)
-{
-	int				ret;
-	char			buf[BUFF_SIZE + 1];
+	char			*buf;
 	char			*tmp;
 	static char		*str;
-	static int		i;
+	int				i;
+// gerer les cas d'erreurs //
+	if (line == NULL || !fd)
+		return (-1);
 
-	ft_bzero(buf, BUFF_SIZE + 1);
-	str = search_n(fd, str)
+// verifier les \n  dans la static str avant de read //
+	i = 0;
+	if ((tmp = ft_strchr(str, '\n')))
+	{
+		while (str[i] != *tmp)
+		{
+			(*line)[i] = str[i];
+			i++;
+		}
+		return (1);
+	}
+
+// lancement boucle read //
+	buf = ft_strnew(BUFF_SIZE);
+	str = ft_strnew(0);
+	while ((ret = read(fd, buf, BUFF_SIZE)) ==  BUFF_SIZE)
+	{
+		i = 0;
+		if ((tmp = ft_strchr(buf, '\n')))
+		{
+			while (buf[i] != *tmp)
+			{
+				(*line)[i] = str[i];
+				i++;
+			}
+			return (1);
+		}
+	}
+	return (0);
+}
+
+		
+/*
+	buf = ft_strnew(BUFF_SIZE);
+	str = ft_strnew(0); // static = init 0 auto (BSS)
 	ret = -1;
-	i = -1;
 	while (ret != 0)
 	{
 		if ((ret = read(fd, buf, BUFF_SIZE)) == -1)
 			return (-1);
-		buf[ret] = '\0'; // apparemment useless
-		tmp = (char*)malloc(ft_strlen(str) + ret);
-		tmp = ft_memcpy(tmp, str, ft_strlen(str) + 1);
-//	:w
-//	ft_strdel(&str);
+//		buf[ret] = '\0';
+		tmp = (char *)malloc(ft_strlen(str) + ret);
+		tmp = ft_strcpy(tmp, str);
+//		ft_strdel(&str);
 		str = ft_strcat(tmp, buf);
-//		ft_strdel(&tmp);
+		ft_strdel(&tmp);
 		pts(str);ptcn;
 	}
 	pts(str);ptcn;
-	return (get_next_line(fd, line);
+	return (0);
 }
 
+*/
 
 
 
