af2bb7bbc5301722a08e7ab285764d8107a8d281
Arthur TOUPART <atoupart@e1r8p11.42.fr>
2016-04-08 16:17:47 +0200

    normalment c'est bon

--- get_next_line.c
+++ get_next_line.c
@@ -12,58 +12,62 @@
 
 #include "get_next_line.h"
 
-int			verif_n(char **str, char **line)
-{
-	char	*tmp;
-
-	if ((tmp = ft_strchr(*str, '\n')))
-	{
-		*line = ft_strsub(*str, 0, tmp - *str);
-		*str = ft_strsub(*str, tmp + 1 - *str, (*str + ft_strlen(*str)) - tmp);
-		return (1);
-	}
-	return (0);
-}
-
-int			inside_while(char **str, char **line, char *buf, int ret)
-{
-	if (!*str)
-		*str = ft_strnew(0);
-	*str = ft_strjoin(*str, buf);
-	if (verif_n(str, line))
-		return (1);
-	else if (ret != BUFF_SIZE)
-	{
-		*line = *str;
-		ft_strdel(str);
-		return (1);
-	}
-	return (0);
-}
-
 int			get_next_line(int const fd, char **line)
 {
 	int				ret;
 	char			*buf;
 	static char		*str;
+	char			*tmp;
 
 	if (line == NULL || fd < 0)
 		return (-1);
-	if (str && verif_n(&str, line))
-		return (1);
-	buf = ft_strnew(BUFF_SIZE);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	if (str && (tmp = ft_strchr(str, '\n')))
 	{
-		if (ret == -1)
-			return (-1);
-		if (inside_while(&str, line, buf, ret))
-			return (1);
+		*line = ft_strsub(str, 0, tmp - str);
+		str = ft_strsub(str, tmp + 1 - str, (str + ft_strlen(str)) - tmp);
+		return (1);
 	}
-	if (str && ret == 0)
+	buf = ft_strnew(BUFF_SIZE);
+while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		if (!str)
+			str = ft_strnew(0);
+
+		str = ft_strjoin(str, buf);
+
+		bzero(buf, BUFF_SIZE);
+
+		if ((tmp = ft_strchr(str, '\n')))
+		{
+			*line = ft_strsub(str, 0, tmp - str);
+
+			buf = ft_strsub(str, tmp + 1 - str, (str + ft_strlen(str)) - tmp);
+			if (*buf != '\0') // viens d'etre rajouter
+			{
+				str = buf;
+			}
+			else
+				ft_strdel(&str);
+			return (1);
+		}
+		else if (ret != BUFF_SIZE)
+		{
+			*line = str;
+
+			ft_strdel(&str);
+			return (1);
+		}
+	}
+
+
+	if (str && ret == 0 && *str != '\n' && *str != '\0')
 	{
 		*line = str;
+
 		ft_strdel(&str);
 		return (1);
-	}
+	}	
+	if (ret == -1)
+		return (-1);
 	return (0);
 }
