c7e5ecd590b2686a70c01527bf62745b48c7cdf5
Arthur TOUPART <atoupart@e1r8p13.42.fr>
2016-04-17 17:47:42 +0200

    gnl.c add anti leaks

--- gnl.c
+++ gnl.c
@@ -16,43 +16,53 @@ int			get_next_line(int const fd, char **line)
 {
 	int				ret;
 	char			*buf;
-	static char		*str;
+	static char		*str[OPEN_MAX];
 	char			*tmp;
 
 	if (line == NULL || fd < 0)
 		return (-1);
-	if (str && (tmp = ft_strchr(str, '\n')))
+	if (str[fd] && (tmp = ft_strchr(str[fd], '\n')))
 	{
-		*line = ft_strsub(str, 0, tmp - str);
-		str = ft_strsub(str, tmp + 1 - str, (str + ft_strlen(str)) - tmp);
+		*line = ft_strsub(str[fd], 0, tmp - str[fd]);
+		str[fd] = ft_strsub_f(&str[fd], tmp + 1 - str[fd], (str[fd] + ft_strlen(str[fd])) - tmp);
 		return (1);
 	}
 	buf = ft_strnew(BUFF_SIZE);
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		if (!str)
-			str = ft_strnew(0);
-		if (ret == -1)
-			return (-1);
-		str = ft_strjoin(str, buf);
-		if ((tmp = ft_strchr(str, '\n')))
+	{	
+		buf[ret] = '\0';
+		if (!str[fd])
+			str[fd] = ft_strnew(0);
+		tmp = str[fd];
+		str[fd] = ft_strjoin(str[fd], buf);
+		ft_strdel(&tmp);
+		if ((tmp = ft_strchr(str[fd], '\n')))
 		{
-			*line = ft_strsub(str, 0, tmp - str);
-			str = ft_strsub(str, tmp + 1 - str, (str + ft_strlen(str)) - tmp);
+			*line = ft_strsub(str[fd], 0, tmp - str[fd]);
+			tmp = ft_strsub_f(&str[fd], tmp + 1 - str[fd], (str[fd] + ft_strlen(str[fd])) - tmp);
+
+			str[fd] = tmp;
+			ft_strdel(&buf);
 			return (1);
 		}
 		else if (ret != BUFF_SIZE)
 		{
-			*line = str;
-			ft_strdel(&str);
+			*line = ft_strdup(str[fd]);
+			ft_strdel(&str[fd]);
+			ft_strdel(&buf);
 			return (1);
 		}
 	}
-	if (str && ret == 0)
+	ft_strdel(&buf);
+	if (str[fd] && ret == 0 && *str[fd] != '\n' && *str[fd] != '\0')
 	{
-		*line = str;
-		ft_strdel(&str);
+		*line = ft_strdup(str[fd]);
+		ft_strdel(&str[fd]);
 		return (1);
 	}
+	if (ret == -1)
+			return (-1);
+
+	ft_strdel(&str[fd]);
 	return (0);
 }
